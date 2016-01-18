3286debbfee86ce9a4607975bada1cdf44823fbe
GregoryJ <GJAccounts@GregoryJerome.com>
2016-01-17 20:14:15 -0600

    Initial commit

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string output = String.Empty;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                output += $"{i} ";
+
+                if (i % 3 == 0)
+                {
+                    output += "Fizz";
+                }
+
+                if (i % 5 == 0)
+                {
+                    output += "Buzz";
+                }
+
+                output += "\n";
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
